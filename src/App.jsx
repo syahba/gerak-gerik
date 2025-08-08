e017660f4ee8faa07914a452b9e05816ec78e048
syahba <ksyaahba@gmail.com>
2025-08-08 21:04:39 +0700

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import WorkoutPage from "./pages/WorkoutPage";
+import FinalPage from "./pages/FinalPage";
+
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/workout" element={<WorkoutPage></WorkoutPage>}></Route>
+        <Route path="/final" element={<FinalPage></FinalPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
